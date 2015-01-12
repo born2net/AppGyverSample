52f299aba7d981c1a05c22a60e973269b630958b
Sean <slevy@mediasignage.com>
2015-01-11 19:14:01 -0800

    some more playing around with appg

--- projectExample/config/structure.coffee
+++ projectExample/config/structure.coffee
@@ -5,6 +5,25 @@ module.exports =
   rootView:
     location: "http://localhost/index.html"
 
+  # See styling options for tabs and other native components in app/common/native-styles/ios.css or app/common/native-styles/android.css
+  tabs: [
+    {
+      title: "Index"
+      id: "index"
+      location: "http://localhost/index.html"
+    }
+    {
+      title: "Internet"
+      id: "internet"
+      location: "http://google.com" # URLs are supported!
+    }
+    {
+      title: "Settings"
+      id: "settings"
+      location: "http://localhost/page4.html"
+    }
+  ]
+
   drawers:
       left:
         id: "leftDrawer"
--- projectExample/www/scripts/application.js
+++ projectExample/www/scripts/application.js
@@ -4,7 +4,7 @@ angular.module('SteroidsApplication', [
 
 $(function () {
     // var view = new supersonic.ui.View("../page2.html");
-
+    var self = this;
     var view = new supersonic.ui.View({
         location: "../page2.html",
         id: "page2"
@@ -16,29 +16,51 @@ $(function () {
     $('#tapMe').on('click', function (e) {
         supersonic.ui.layers.push(view)
     });
-    setTimeout(function(){
+
+    $().on('click', function () {
+        if (counter % 2) {
+            $(this).css({backgroundColor: 'red'});
+        } else {
+            $(this).css({backgroundColor: 'white'});
+        }
+    });
+
+    $('#myHomeButton,#myHomeButton2').on('click', function () {
+        $(this).css({backgroundColor: '#00B4FE'}).delay(100).queue(function(){
+            $(this).css({backgroundColor: 'white'});
+            $(this).dequeue();
+        });
+    });
+
+
+    setTimeout(function () {
         var leftDrawer = new steroids.views.WebView("drawer.html");
+
         function updateDrawer() {
             steroids.drawers.update({
                 left: leftDrawer
             });
         }
-        leftDrawer.preload({},{
+
+        leftDrawer.preload({}, {
             onSuccess: updateDrawer
         });
-    },2000);
 
-    setTimeout(function(){
+        supersonic.ui.tabs.show();
+
+    }, 2000);
+
+    setTimeout(function () {
         supersonic.logger.log('sending...');
         var msg = {
             from: 'app',
             data: 'message 1'
         };
         supersonic.data.channel('events').publish(msg);
-    },6000);
+    }, 6000);
 
-    var unsubscribe = supersonic.data.channel('events').subscribe( function(message, reply) {
-        if (message.from=='app')
+    var unsubscribe = supersonic.data.channel('events').subscribe(function (message, reply) {
+        if (message.from == 'app')
             return;
         alert('from : ' + message.from + ' ' + message.data);
     });
